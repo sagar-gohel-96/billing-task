fa7757365271dc49a6eea3970530e2a6045fdf48
sagar-gohel-96 <sagar.180180107011@gmail.com>
2023-09-29 14:27:19 +0530

    fix  : router to billing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import Box from "@mui/material/Box";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export interface SidebarItemType {
   icon: React.ReactNode;
@@ -7,6 +9,11 @@ export interface SidebarItemType {
 }
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/billing");
+  }, [router]);
+
   return (
     <Box sx={{ display: "flex", height: "100vh" }}>
       <main>Hello</main>
